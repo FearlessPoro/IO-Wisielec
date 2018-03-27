ac1a96bb7d0d88eab03f5bccb4543e906acdfd63
Paweł Hadam <pawel.hadam19@gmail.com>
2018-03-27 18:07:39 +0200

    Add HangManEntity with tests

---
+++ src/test/java/entity/HangManEntityTest.java
@@ -0,0 +1,36 @@
+package entity;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class HangManEntityTest {
+
+    private HangManEntity testEntity;
+
+    @Before
+    public void setUp() throws Exception {
+        testEntity = new HangManEntity();
+    }
+
+    @Test
+    public void getHearths() {
+        int expectedLives = 10;
+
+        int resultLives = testEntity.getHearths();
+
+        Assert.assertEquals(expectedLives, resultLives);
+    }
+
+    @Test
+    public void decrementLives() {
+        int expectedLives = 9;
+
+        testEntity.decrementLives();
+        int resultLives = testEntity.getHearths();
+
+        Assert.assertEquals(expectedLives, resultLives);
+    }
+}

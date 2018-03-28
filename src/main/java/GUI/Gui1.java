d61a7b5fa798132b9f00e00c5ed0f75d2d47e54a
JustynaGacek <justynagacek6@gmail.com>
2018-03-28 18:10:54 +0200

    Added GUI.

---
+++ src/main/java/GUI/Gui1.java
@@ -0,0 +1,247 @@
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextArea;
+import javafx.scene.layout.Pane;
+
+public class Gui1 {
+
+    @FXML
+    private ResourceBundle resources;
+
+    @FXML
+    private URL location;
+
+    @FXML
+    private Pane keyboardPane;
+
+    @FXML
+    private Pane passwordPane;
+
+    @FXML
+    private TextArea guessPasswordField;
+
+    @FXML
+    private TextArea wholePasswordField;
+
+    @FXML
+    private Pane hangedManPane;
+
+    @FXML
+    private Label winMessageLabel;
+
+    @FXML
+    private Label leftChanceLabel;
+
+    @FXML
+    private Pane randomPasswordPane;
+
+    @FXML
+    private Pane saveAndExitPane;
+
+    @FXML
+    void a1Click(ActionEvent event) {
+
+    }
+
+    @FXML
+    void aClick(ActionEvent event) {
+
+    }
+
+    @FXML
+    void bClick(ActionEvent event) {
+
+    }
+
+    @FXML
+    void c1Click(ActionEvent event) {
+
+    }
+
+    @FXML
+    void cClick(ActionEvent event) {
+
+    }
+
+    @FXML
+    void checkPasswordAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void dClick(ActionEvent event) {
+
+    }
+
+    @FXML
+    void e1Click(ActionEvent event) {
+
+    }
+
+    @FXML
+    void eClick(ActionEvent event) {
+
+    }
+
+    @FXML
+    void fClick(ActionEvent event) {
+
+    }
+
+    @FXML
+    void gClick(ActionEvent event) {
+
+    }
+
+    @FXML
+    void hClick(ActionEvent event) {
+
+    }
+
+    @FXML
+    void iClick(ActionEvent event) {
+
+    }
+
+    @FXML
+    void jClick(ActionEvent event) {
+
+    }
+
+    @FXML
+    void kClick(ActionEvent event) {
+
+    }
+
+    @FXML
+    void l1Click(ActionEvent event) {
+
+    }
+
+    @FXML
+    void lClick(ActionEvent event) {
+
+    }
+
+    @FXML
+    void mClick(ActionEvent event) {
+
+    }
+
+    @FXML
+    void n1Click(ActionEvent event) {
+
+    }
+
+    @FXML
+    void nClick(ActionEvent event) {
+
+    }
+
+    @FXML
+    void o1Click(ActionEvent event) {
+
+    }
+
+    @FXML
+    void oClick(ActionEvent event) {
+
+    }
+
+    @FXML
+    void pClick(ActionEvent event) {
+
+    }
+
+    @FXML
+    void qClick(ActionEvent event) {
+
+    }
+
+    @FXML
+    void rClick(ActionEvent event) {
+
+    }
+
+    @FXML
+    void randomPasswordAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void s1Click(ActionEvent event) {
+
+    }
+
+    @FXML
+    void sClick(ActionEvent event) {
+
+    }
+
+    @FXML
+    void saveAndExitAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void tClick(ActionEvent event) {
+
+    }
+
+    @FXML
+    void uClick(ActionEvent event) {
+
+    }
+
+    @FXML
+    void vClick(ActionEvent event) {
+
+    }
+
+    @FXML
+    void wClick(ActionEvent event) {
+
+    }
+
+    @FXML
+    void xClick(ActionEvent event) {
+
+    }
+
+    @FXML
+    void yClick(ActionEvent event) {
+
+    }
+
+    @FXML
+    void z1Click(ActionEvent event) {
+
+    }
+
+    @FXML
+    void z2Click(ActionEvent event) {
+
+    }
+
+    @FXML
+    void zClick(ActionEvent event) {
+
+    }
+
+    @FXML
+    void initialize() {
+        assert keyboardPane != null : "fx:id=\"keyboardPane\" was not injected: check your FXML file 'Gui1.fxml'.";
+        assert passwordPane != null : "fx:id=\"passwordPane\" was not injected: check your FXML file 'Gui1.fxml'.";
+        assert guessPasswordField != null : "fx:id=\"guessPasswordField\" was not injected: check your FXML file 'Gui1.fxml'.";
+        assert wholePasswordField != null : "fx:id=\"wholePasswordField\" was not injected: check your FXML file 'Gui1.fxml'.";
+        assert hangedManPane != null : "fx:id=\"hangedManPane\" was not injected: check your FXML file 'Gui1.fxml'.";
+        assert winMessageLabel != null : "fx:id=\"winMessageLabel\" was not injected: check your FXML file 'Gui1.fxml'.";
+        assert leftChanceLabel != null : "fx:id=\"leftChanceLabel\" was not injected: check your FXML file 'Gui1.fxml'.";
+        assert randomPasswordPane != null : "fx:id=\"randomPasswordPane\" was not injected: check your FXML file 'Gui1.fxml'.";
+        assert saveAndExitPane != null : "fx:id=\"saveAndExitPane\" was not injected: check your FXML file 'Gui1.fxml'.";
+
+    }
+}

47990d42d7fda2136220ef303c330e484d57144d
czaduu <adam0512>
2018-04-03 20:57:00 +0200

    added some tests

--- src/main/java/HangMan.java
+++ src/main/java/HangMan.java
@@ -4,5 +4,6 @@ public class HangMan {
 
     public static void main(String[] args){
         Game game = new Game();
+        game.start();
     }
 }
--- src/main/java/entity/WordEntity.java
+++ src/main/java/entity/WordEntity.java
@@ -1,21 +1,23 @@
 package entity;
 
-import java.util.Arrays;
+import java.util.ArrayList;
 
 public class WordEntity {
 
     private String wordToGuess;
     private char[] unknownWord;
+    private ArrayList<Character> selectedLetters;
 
     public WordEntity(String wordToGuess) {
         this.wordToGuess = wordToGuess;
-
-        unknownWord = new char[wordToGuess.length()];
-        Arrays.fill(unknownWord, '_');
-    }
-
-    public void revealLetter(int index) {
-        unknownWord[index] = wordToGuess.charAt(index);
+        this.unknownWord = new char[wordToGuess.length()];
+        this.selectedLetters = new ArrayList<>();
+        for(int i = 0; i < wordToGuess.length(); ++i) {
+            if (Character.isWhitespace(wordToGuess.charAt(i)))
+                unknownWord[i] = ' ';
+            else
+                unknownWord[i] = '_';
+        }
     }
 
     public String getWordToGuess() {
@@ -25,4 +27,37 @@ public class WordEntity {
     public char[] getUnknownWord() {
         return unknownWord;
     }
+
+
+    public boolean alreadySelectedLetter(Character character){
+        return selectedLetters.contains(character);
+    }
+
+    public void addToAlreadySelectedLetter(Character character){
+        selectedLetters.add(character);
+    }
+
+    public boolean ifWordToGuessContainsLetter(char character){
+        boolean contains = false;
+        for(int i = 0, n = this.wordToGuess.length() ; i < n ; i++) {
+            char c = this.wordToGuess.charAt(i);
+            if(c == character){
+                revealLetter(i);
+                contains = true;
+            }
+        }
+        return contains;
+    }
+
+    public boolean allLettersRevealed() {
+        for(char c : unknownWord)
+            if(c == '_')
+                return false;
+        return true;
+    }
+
+    private void revealLetter(int index) {
+        unknownWord[index] = wordToGuess.charAt(index);
+    }
+
 }

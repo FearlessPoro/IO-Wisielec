37b1bf85ff7d2ea1589ab9bee33c2d39d214a3bf
Pawel Hadam <pawel.hadam19@gmail.com>
2018-04-23 17:16:25 +0200

    Add proper sounds

--- SRC/src/main/java/logic/SoundEffect.java
+++ SRC/src/main/java/logic/SoundEffect.java
@@ -3,26 +3,23 @@ package logic;
 import javax.sound.sampled.*;
 import java.io.IOException;
 import java.net.URL;
+import java.util.Objects;
 
 public enum SoundEffect {
-    BAD_LETTER("sounds/battle003.wav"),
-    GOOD_LETTER("sounds/battle003.wav"),
-    GAME_LOST("sounds/battle003.wav"),
-    GAME_WON("sounds/battle003.wav");
+    BAD_LETTER("sounds/wrong.wav"),
+    GOOD_LETTER("sounds/good.wav"),
+    GAME_LOST("sounds/lost.wav"),
+    GAME_WON("sounds/won.wav");
 
     private Clip clip;
 
     SoundEffect(String soundFileName) {
         try {
             URL url = this.getClass().getClassLoader().getResource(soundFileName);
-            AudioInputStream audioInputStream = AudioSystem.getAudioInputStream(url);
+            AudioInputStream audioInputStream = AudioSystem.getAudioInputStream(Objects.requireNonNull(url));
             clip = AudioSystem.getClip();
             clip.open(audioInputStream);
-        } catch (UnsupportedAudioFileException e) {
-            e.printStackTrace();
-        } catch (IOException e) {
-            e.printStackTrace();
-        } catch (LineUnavailableException e) {
+        } catch (UnsupportedAudioFileException | IOException | LineUnavailableException e) {
             e.printStackTrace();
         }
     }

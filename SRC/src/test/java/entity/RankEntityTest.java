ea1a5397e1f7891189ff46854142b6ad46636966
Fr4gtastic <milosz.swierad@gmail.com>
2018-05-30 15:32:09 +0200

    First tests of RankEntity

---
+++ SRC/src/test/java/entity/RankEntityTest.java
@@ -0,0 +1,25 @@
+package entity;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+public class RankEntityTest {
+
+    private RankEntity testEntity;
+    private String testWord = "test";
+    private Integer testPoints = 10;
+    private RankEntity.Record testRecord = new RankEntity.Record(testPoints, testWord);
+
+    @Before
+    public void setUp() {
+        testEntity = new RankEntity();
+    }
+
+    @Test
+    public void addToRankTest() {
+        testEntity.addToRank(testPoints, testWord);
+        Assert.assertEquals(testEntity.getScoreAtSpecificIndex(0), testRecord);
+    }
+
+}
